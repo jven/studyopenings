f82e7c3a8172fa3cfe905f4f98c3ece6d0aee82c
Justin Venezuela <jven@jvenezue.la>
2018-11-19 10:02:17 -0500

    Adds a database path variable.

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
+const Config = require('./server/config.js').Config;
 
 app.use(express.static(path.join(__dirname, 'client')));
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log('Listening on ' + port + '.'));
+server.listen(port, () => {
+  console.log('studyopenings is running!');
+  
+  console.log('Listening on ' + port + '.');
+  const databasePath = process.env.DATABASE_PATH || Config.DATABASE_PATH;
+  console.log('Database path: ' + databasePath);
+});
---
+++ server/config.js
@@ -0,0 +1,5 @@
+const Config = {
+  DATABASE_PATH: 'My database path!'
+}
+
+exports.Config = Config;
