f85d0d16c772b0dcbcdf0ca2dc9963c73d613d59
Justin Venezuela <jven@jvenezue.la>
2018-12-18 17:30:21 -0500

    Migrates Line to TypeScript.

--- src/client/js/study/line.ts
+++ src/client/js/study/line.ts
@@ -1,29 +1,34 @@
 import { Color } from '../../../protocol/color';
 import { Move } from '../common/move';
 
+declare var Chess: any;
+
 export class Line {
+  public startPosition: string;
+  public opponentFirstMove: Move | null;
+  public moves: Move[];
+  public color: Color;
+
   constructor(
-      startPosition,
-      opponentFirstMove,
-      moves,
-      color) {
+      startPosition: string,
+      opponentFirstMove: Move | null,
+      moves: Move[],
+      color: Color) {
     this.startPosition = startPosition;
     this.opponentFirstMove = opponentFirstMove;
     this.moves = moves;
     this.color = color;
   }
 
-  static fromPgnForInitialPosition(pgn, color) {
+  static fromPgnForInitialPosition(pgn: string, color: Color) {
     var chess = new Chess();
     if (!chess.load_pgn(pgn)) {
-      console.error('Invalid PGN: ' + pgn);
-      return null;
+      throw new Error('Invalid PGN: ' + pgn);
     }
 
     var opponentFirstMove = null;
-    var moves = chess
-        .history({verbose: true})
-        .map(m => new Move(m.from, m.to));
+    var history: {from: string, to: string}[] = chess.history({verbose: true});
+    var moves = history.map(m => new Move(m.from, m.to));
     if (color == Color.BLACK) {
       opponentFirstMove = moves.splice(0, 1)[0];
     }
@@ -34,7 +39,8 @@ export class Line {
         color);
   }
 
-  static fromMoveStringsForInitialPosition(moveStrings, color) {
+  static fromMoveStringsForInitialPosition(
+      moveStrings: string[], color: Color) {
     var pgn = '';
     for (var i = 0; i < moveStrings.length; i += 2) {
       pgn += (i / 2 + 1) + '. ' + moveStrings[i] + ' ';
