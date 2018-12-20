485736537127d75bb6c1d383be78035e7e364ae5
Justin Venezuela <jven@jvenezue.la>
2018-12-20 16:06:24 -0500

    Removes unnecessary JS libraries. These are now pulled into the JS bundle.

--- src/client/js/common/toasts.ts
+++ src/client/js/common/toasts.ts
@@ -1,14 +1,16 @@
+import { error, options } from 'toastr';
+
 export class Toasts {
   static initialize() {
-    toastr.options.showDuration = 300;
-    toastr.options.hideDuration = 300;
-    toastr.options.timeOut = 0;
-    toastr.options.extendedTimeOut = 0;
-    toastr.options.preventDuplicates = true;
-    toastr.options.positionClass = "toast-top-center";
+    options.showDuration = 300;
+    options.hideDuration = 300;
+    options.timeOut = 0;
+    options.extendedTimeOut = 0;
+    options.preventDuplicates = true;
+    options.positionClass = "toast-top-center";
   }
 
   static error(title: string, message: string) {
-    toastr.error(message, title);
+    error(message, title);
   }
 }
