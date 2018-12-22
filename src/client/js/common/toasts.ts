bc7fd36f0142decb3b4fa0748d3cec8561463645
Justin Venezuela <jven@jvenezue.la>
2018-12-22 01:11:08 -0500

    Puts JS libraries back as separate files instead of including them in the bundle. They unnecessarily bloat the bundle size.

--- src/client/js/common/toasts.ts
+++ src/client/js/common/toasts.ts
@@ -1,16 +1,14 @@
-import { error, options } from 'toastr';
-
 export class Toasts {
   static initialize() {
-    options.showDuration = 300;
-    options.hideDuration = 300;
-    options.timeOut = 0;
-    options.extendedTimeOut = 0;
-    options.preventDuplicates = true;
-    options.positionClass = "toast-top-center";
+    toastr.options.showDuration = 300;
+    toastr.options.hideDuration = 300;
+    toastr.options.timeOut = 0;
+    toastr.options.extendedTimeOut = 0;
+    toastr.options.preventDuplicates = true;
+    toastr.options.positionClass = "toast-top-center";
   }
 
   static error(title: string, message: string) {
-    error(message, title);
+    toastr.error(message, title);
   }
 }
