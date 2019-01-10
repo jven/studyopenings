1010c65ce6a83188c71a52e39612280f6800b920
Justin Venezuela <jven@jvenezue.la>
2019-01-10 15:41:17 -0500

    Changes the title of imported repertoires from 'Imported repertoire' to 'PGN imported on ${DATE} ${TIME}'.

--- src/client/js/build/import/treemodelpopulator.ts
+++ src/client/js/build/import/treemodelpopulator.ts
@@ -1,14 +1,17 @@
 import { ParsedVariation } from "./pgnparser";
 import { TreeModel } from "../../tree/treemodel";
 import { assert } from "../../../../util/assert";
+import { getUtcDate, getUtcTime } from "../../../../util/datetime";
 
 export class TreeModelPopulator {
   private treeModel_: TreeModel;
   private pendingOperations_: Operation[];
 
   constructor(mainLineVariation: ParsedVariation) {
+    const now = new Date();
     this.treeModel_ = new TreeModel();
-    this.treeModel_.setRepertoireName('Imported repertoire');
+    this.treeModel_.setRepertoireName(
+        `PGN imported on ${getUtcDate(now)} ${getUtcTime(now)}`);
     
     this.pendingOperations_ = [];
     if (mainLineVariation.moves.length) {
