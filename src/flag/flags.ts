377d89bf3c87a6432785210f7e0105ca0b004da8
Justin Venezuela <jven@jvenezue.la>
2019-01-18 17:31:44 -0500

    Adds an ENABLE_BOARD_THEME_TOGGLER flag.

--- src/flag/flags.ts
+++ src/flag/flags.ts
@@ -1,6 +1,10 @@
 import { RolloutState } from './rolloutstate';
 
 export enum FlagName {
+  ENABLE_BOARD_THEME_TOGGLER = 'enable_board_theme_togger'
 }
 
 export const FLAG_MAP: Map<FlagName, RolloutState> = new Map();
+FLAG_MAP.set(
+    FlagName.ENABLE_BOARD_THEME_TOGGLER,
+    RolloutState.LOCAL_ONLY);
