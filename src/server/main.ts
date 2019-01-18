07a65defaf876b02fb913abcf95843d463ae22ea
Justin Venezuela <jven@jvenezue.la>
2019-01-18 13:01:18 -0500

    Extracts server initialization logic into a separate Server class.

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,66 +1,16 @@
+import { Server } from './server';
+
 require('dotenv').config();
-
-import * as createApplication from 'express';
-import { CreateRepertoireAction } from './actions/createrepertoireaction';
-import { DeleteRepertoireAction } from './actions/deleterepertoireaction';
-import { EvaluateFlagsAction } from './actions/evaluateflagsaction';
-import { LoadRepertoireAction } from './actions/loadrepertoireaction';
-import { LogImpressionsAction } from './actions/logimpressionsaction';
-import { RepertoireMetadataAction } from './actions/repertoiremetadataaction';
-import { UpdateRepertoireAction } from './actions/updaterepertoireaction';
-import { DatabaseWrapper } from './databasewrapper';
-import { EndpointRegistry } from './endpointregistry';
-
-const app = createApplication();
-const server = require('http').createServer(app);
-const databaseWrapper = new DatabaseWrapper();
-const endpointRegistry = new EndpointRegistry(app, '1mb');
-
-
-endpointRegistry
-    .registerStaticFolder('../client')
-    .registerStaticFile('/', '../client/main.html')
-    .registerStaticFile('/about', '../client/about.html')
-    .registerLoggedInAction(
-        '/metadata',
-        new RepertoireMetadataAction(databaseWrapper),
-        ['read:repertoires'])
-    .registerLoggedInAction(
-        '/loadrepertoire',
-        new LoadRepertoireAction(databaseWrapper),
-        ['read:repertoires'])
-    .registerLoggedInAction(
-        '/updaterepertoire',
-        new UpdateRepertoireAction(databaseWrapper),
-        ['write:repertoires'])
-    .registerLoggedInAction(
-        '/createrepertoire',
-        new CreateRepertoireAction(databaseWrapper),
-        ['write:repertoires'])
-    .registerLoggedInAction(
-        '/deleterepertoire',
-        new DeleteRepertoireAction(databaseWrapper),
-        ['write:repertoires'])
-    .registerAnonymousAction(
-        '/flags',
-        new EvaluateFlagsAction())
-    .registerAnonymousAction(
-        '/impressions',
-        new LogImpressionsAction(databaseWrapper));
 
 
 function main() {
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || '5000';
   const databasePath = process.env.DATABASE_PATH;
   if (!databasePath) {
     throw new Error('Database path not provided!');
   }
-  server.listen(port, () => {
-    console.log('studyopenings is running!');
 
-    console.log('Listening on ' + port + '.');
-    databaseWrapper.connect(databasePath);
-  });
+  new Server().run(port, databasePath);
 }
 
 
---
+++ src/server/server.ts
@@ -0,0 +1,60 @@
+import * as createApplication from 'express';
+import { createServer, Server as HttpServer } from 'http';
+import { CreateRepertoireAction } from './actions/createrepertoireaction';
+import { DeleteRepertoireAction } from './actions/deleterepertoireaction';
+import { EvaluateFlagsAction } from './actions/evaluateflagsaction';
+import { LoadRepertoireAction } from './actions/loadrepertoireaction';
+import { LogImpressionsAction } from './actions/logimpressionsaction';
+import { RepertoireMetadataAction } from './actions/repertoiremetadataaction';
+import { UpdateRepertoireAction } from './actions/updaterepertoireaction';
+import { DatabaseWrapper } from './databasewrapper';
+import { EndpointRegistry } from './endpointregistry';
+
+export class Server {
+  private httpServer_: HttpServer;
+  private databaseWrapper_: DatabaseWrapper;
+
+  constructor() {
+    const app = createApplication();
+    this.httpServer_ = createServer(app);
+    this.databaseWrapper_ = new DatabaseWrapper();
+
+    new EndpointRegistry(app, '1mb')
+        .registerStaticFolder('../client')
+        .registerStaticFile('/', '../client/main.html')
+        .registerStaticFile('/about', '../client/about.html')
+        .registerLoggedInAction(
+            '/metadata',
+            new RepertoireMetadataAction(this.databaseWrapper_),
+            ['read:repertoires'])
+        .registerLoggedInAction(
+            '/loadrepertoire',
+            new LoadRepertoireAction(this.databaseWrapper_),
+            ['read:repertoires'])
+        .registerLoggedInAction(
+            '/updaterepertoire',
+            new UpdateRepertoireAction(this.databaseWrapper_),
+            ['write:repertoires'])
+        .registerLoggedInAction(
+            '/createrepertoire',
+            new CreateRepertoireAction(this.databaseWrapper_),
+            ['write:repertoires'])
+        .registerLoggedInAction(
+            '/deleterepertoire',
+            new DeleteRepertoireAction(this.databaseWrapper_),
+            ['write:repertoires'])
+        .registerAnonymousAction(
+            '/flags',
+            new EvaluateFlagsAction())
+        .registerAnonymousAction(
+            '/impressions',
+            new LogImpressionsAction(this.databaseWrapper_));
+  }
+
+  run(port: string, databasePath: string): void {
+    this.httpServer_.listen(port, () => {
+      console.log(`StudyOpenings is running!\nListening on port ${port}...`);
+      this.databaseWrapper_.connect(databasePath);
+    });
+  }
+}
