748ffc6f88ce8fc58cc6f9c7a4995b46306def10
Justin Venezuela <jven@jvenezue.la>
2019-01-18 15:50:12 -0500

    Imports crypto-random-string using import rather than var require.

--- src/util/random.ts
+++ src/util/random.ts
@@ -1,4 +1,4 @@
-const cryptoRandomString = require('crypto-random-string');
+import cryptoRandomString = require('crypto-random-string');
 
 export function getRandomString(length: number): string {
   return cryptoRandomString(length);
