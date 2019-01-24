548a1189e51a070869c058a61699624c9d9b731d
Justin Venezuela <jven@jvenezue.la>
2019-01-24 17:39:05 -0500

    Enables the ENABLE_PICKER_DELETE_CONFIRM flag everywhere.

--- src/flag/flags.ts
+++ src/flag/flags.ts
@@ -8,4 +8,4 @@ export const FLAG_MAP: Map<FlagName, RolloutState> = new Map();
 FLAG_MAP
     .set(
         FlagName.ENABLE_PICKER_DELETE_CONFIRM,
-        RolloutState.LOCAL_ONLY);
+        RolloutState.ENABLED_EVERYWHERE);
