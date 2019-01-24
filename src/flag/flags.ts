55554c29a4d036fcc7c5d274d9e3b1fa1dc68257
Justin Venezuela <jven@jvenezue.la>
2019-01-24 16:54:43 -0500

    Adds an ENABLE_PICKER_DELETE_CONFIRM flag.

--- src/flag/flags.ts
+++ src/flag/flags.ts
@@ -1,6 +1,11 @@
 import { RolloutState } from './rolloutstate';
 
 export enum FlagName {
+  ENABLE_PICKER_DELETE_CONFIRM = 'enable_picker_delete_confirm'
 }
 
 export const FLAG_MAP: Map<FlagName, RolloutState> = new Map();
+FLAG_MAP
+    .set(
+        FlagName.ENABLE_PICKER_DELETE_CONFIRM,
+        RolloutState.LOCAL_ONLY);
