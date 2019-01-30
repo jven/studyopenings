b27c67007f0e2319530b35d762efe1eabda947cc
Justin Venezuela <jven@jvenezue.la>
2019-01-29 20:51:21 -0500

    Adds a new ENABLE_EVALUATE_MODE flag.

--- src/flag/flags.ts
+++ src/flag/flags.ts
@@ -1,11 +1,15 @@
 import { RolloutState } from './rolloutstate';
 
 export enum FlagName {
+  ENABLE_EVALUATE_MODE = 'enable_evaluate_mode',
   ENABLE_PICKER_DELETE_CONFIRM = 'enable_picker_delete_confirm'
 }
 
 export const FLAG_MAP: Map<FlagName, RolloutState> = new Map();
 FLAG_MAP
+    .set(
+      FlagName.ENABLE_EVALUATE_MODE,
+        RolloutState.LOCAL_ONLY)
     .set(
         FlagName.ENABLE_PICKER_DELETE_CONFIRM,
         RolloutState.ENABLED_EVERYWHERE);
